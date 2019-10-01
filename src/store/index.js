351c338c3c3c352261fbfe7d96323ba49f94a54e
Flavio Tavares Silva <flavio.silva@rodassist.com.br>
2019-10-01 18:08:31 -0300

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux';
+
+function reducer() {
+  return {
+    activeLessons: null,
+    activeModules: null,
+    modules: [
+      { 
+        id: 1, title: 'Iniciando com react', lessons: [
+          { id: 1, title: 'Primeira aula' },
+          { id: 2, title: 'Segunda aula' }
+        ]
+      },
+      {
+        id: 2, title: 'Aprendendo redux', lessons: [
+          { id: 3, title: 'Terceira aula' },
+          { id: 4, title: 'Quarta aula' },
+        ],
+      }
+    ]
+  }
+}
+
+const store = createStore(reducer);
+
+export default store;
